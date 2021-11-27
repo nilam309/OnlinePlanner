94b55f2acef0086ebe38c459806db03618411871
x19226993 <bhushan61941@gmail.com>
2021-11-27 12:09:06 +0000

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using OnlinePlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlinePlanner.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private OnlinePlannerEntities1 db = new OnlinePlannerEntities1();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OnlinePlannerEntities1 db = new OnlinePlannerEntities1())
+                {
+                    if(user.UserName=="Nilam" && user.Password=="Nilam123")
+                    {
+                        Session["UserID"] = 1;
+                        Session["UserName"] = "Nilam";
+                        Session["Password"] = "Nilam123";
+                        return RedirectToAction("UserDashBoard");
+                    }
+                    else
+                    {
+                        var obj = db.Users.Where(a => a.UserName.Equals(user.UserName) && a.Password.Equals(user.Password)).FirstOrDefault();
+                        if (obj != null)
+                        {
+                            //maintaining user details in the session for the later use
+                            Session["UserID"] = obj.UserID.ToString();
+                            Session["UserName"] = obj.UserName.ToString();
+                            Session["Password"] = obj.Password.ToString();
+                            return RedirectToAction("UserDashBoard");
+                        }
+                    }
+                   
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return RedirectToAction("List", "Task");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult LogOff()
+        {
+            Session.Clear();
+            Session.Abandon();
+            // Redirecting to Login page after deleting Session
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ Controllers/TaskController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using OnlinePlanner.Models;
+using System.Net;
+
+namespace OnlinePlanner.Controllers
+{
+    public class TaskController : Controller
+    {
+
+        private OnlinePlannerEntities1 db = new OnlinePlannerEntities1();
+        // GET: Task
+        public ActionResult List()
+        {
+            // Query to entity for active record and created by logged in User
+            int userID =Convert.ToInt32 (Session["UserID"]);
+            var studentList = db.Tasks.SqlQuery("Select * from Task where IsDeleted=0 and CreatedBy="+userID)
+                      .ToList<Task>();
+            return View(studentList);
+        }
+
+        // GET: course/Create
+        public ActionResult AddTask()
+        {
+            return View();
+        }
+
+        // POST: course/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddTask( Task task)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    task.CreatedBy= Convert.ToInt32(Session["UserID"]);
+                    db.Tasks.Add(task);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View(task);
+        }
+
+        public ActionResult EditTask(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditTask(Task task)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Edit task will save date as a last updated date
+                    task.LastUpdated = Convert.ToString(DateTime.Now);
+                    db.Entry(task).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return View(task);
+        }
+
+
+        // GET: course/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                //Softdelete,by setting IsDeleted flag true 
+                task.IsDeleted = true;
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+        }
+
+       
+   
+        
+    }
+}
